447b332b41ddffdeeaa1a07164107d77b1f8d7a8
priyankar2799 <128132877+priyankar2799@users.noreply.github.com>
2024-03-13 22:23:57 +0530

    Update main.py

--- python-web-app/main.py
+++ python-web-app/main.py
@@ -7,7 +7,7 @@ app = flask(__name__)
 #route defines the root of url, when the user vists the root url flask will call the function and returns the string
 @app.route('/success/<name>')
 def success(name):
- return 'Hello %s', % name 
+ return 'Hello %s' % name 
   
 #This code defines a route /login that accepts both POST and GET requests. If the request method is POST, it retrieves the value of the nm form field submitted by the user, and redirects the user to the /success/<name> route using redirect(url_for('success', name=user)). If the request method is GET, it retrieves the value of the nm query parameter from the URL and performs the same redirection.
 @app.route('/login' , methods['POST','GET'] )
